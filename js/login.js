f7de766d73ed49c4111a562163cbe6aed60f5292
Mayu-kh <115211252+Mayu-kh@users.noreply.github.com>
2022-10-15 12:36:42 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+function validate(){
+    let username=document.getElementById('username').value;
+    let password=document.getElementById('password').value;
+    
+if(username=='mayu@gmail.com' && password =='mayu123')
+{
+    alert('Login Successful!!');
+    window.location.href='third.html';
+}else{
+    alert('Login Alert!!');
+}
+
+}
